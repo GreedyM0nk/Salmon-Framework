4dd14801675dc64439b550107366502e36eed55c
gaurav <gkarvir@salmon.com>
2014-10-21 16:01:38 +0100

    Added sl4j simple

--- src/test/java/com/salmon/test/pageobjects/HomeSamplePage.java
+++ src/test/java/com/salmon/test/pageobjects/HomeSamplePage.java
@@ -2,14 +2,12 @@ package com.salmon.test.pageobjects;
 
 import com.salmon.test.framework.PageObject;
 import org.openqa.selenium.By;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /*Sample page object class which defines all the elements for a specific page.
 * Every  page object class should extend "PageObject" class
 * */
 public class HomeSamplePage extends PageObject {
-    protected static final Logger LOG = LoggerFactory.getLogger(HomeSamplePage.class);
+    //protected static final Logger LOG = LoggerFactory.getLogger(HomeSamplePage.class);
 
     private  By headerSignInLink = By.id("headerSignInLink");
 
--- src/test/java/com/salmon/test/pageobjects/RegistrationSamplePage.java
+++ src/test/java/com/salmon/test/pageobjects/RegistrationSamplePage.java
@@ -1,12 +1,6 @@
 package com.salmon.test.pageobjects;
 
 import com.salmon.test.framework.PageObject;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 /*Sample page object class which defines all the elements for a specific page.
 * Every  page object class should extend "PageObject" class
@@ -15,21 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RegistrationSamplePage extends PageObject {
 
-    public RegistrationSamplePage( ) {
-    RegistrationSamplePage registrationSamplePage = PageFactory.initElements(webDriver, RegistrationSamplePage.class);
-    }
 
-    @FindBy(id = "headerSignInLink")
-    private WebElement headerSignInLinkUsingFindBy;
-
-
-
-    public void getHeaderSignInLinkUsingFindBy(){
-        WebDriverWait wait = new WebDriverWait(webDriver, 10);
-        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.id("headerSignInLink")));
-        headerSignInLinkUsingFindBy.click();
-
-    }
 
 
 
