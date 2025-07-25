8be61386ffc7a1552fd0aa38f131943b5847f56f
Souvik <insomniacsouvik@outlook.com>
2025-07-25 21:18:27 +0530

    refactoring version 3

--- src/test/java/com/salmon/test/TestRunJenkinsSuite.java
+++ src/test/java/com/salmon/test/TestRunJenkinsSuite.java
@@ -1,11 +1,24 @@
 package com.salmon.test;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-
-@CucumberOptions(features = "target/test-classes", tags = "@gui,@api", monochrome = true, plugin = {
-		"pretty", "html:target/cucumber-report/runjenkins",
-		"json:target/cucumber-report/runjenkins/cucumber.json" })
-public class TestRunJenkinsSuite extends  AbstractTestNGCucumberTests{
+/**
+ * TestNG runner for Jenkins automation suite using Cucumber.
+ * This runner executes both GUI and API tests with tags "@gui" and "@api".
+ * Reports are generated in HTML and JSON formats under target/cucumber-report/runjenkins.
+ */
+@CucumberOptions(
+    features = "target/test-classes", // Path to compiled feature files
+    glue = "com.salmon.test.step_definitions", // Step definitions package
+    tags = "@gui,@api", // Run scenarios tagged with @gui or @api
+    monochrome = true, // Cleaner console output
+    plugin = {
+        "pretty",
+        "html:target/cucumber-report/runjenkins",
+        "json:target/cucumber-report/runjenkins/cucumber.json"
+    }
+)
+public class TestRunJenkinsSuite extends AbstractTestNGCucumberTests {
+    // No additional code needed; Cucumber and TestNG handle execution.
 }
--- src/test/java/com/salmon/test/framework/helpers/LoadProperties.java
+++ src/test/java/com/salmon/test/framework/helpers/LoadProperties.java
@@ -1,71 +1,27 @@
 package com.salmon.test.framework.helpers;
 
-import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
-
-//public  class LoadProperties {
-//    private static  final Logger LOG = LoggerFactory.getLogger(LoadProperties.class);
-//    @Getter
-//    private static Properties runProps;
-//
-//    public static void loadRunConfigProps(String configPropertyFileLocation) {
-//
-//        runProps = new Properties();
-//        try (InputStream inputStream = LoadProperties.class.getResourceAsStream(configPropertyFileLocation)) {
-//            runProps.load(inputStream);
-//            setUpEnvironmentURLFor("site.url");
-//            setUpEnvironmentURLFor("api.url");
-//            setUpEnvironmentURLFor("site.port");
-//            setUpEnvironmentURLFor("site.basepath");
-//            setUpEnvironmentURLFor("browser");
-//            setUpEnvironmentURLFor("platform");
-//            setUpEnvironmentURLFor("driver.root.dir");
-//        } catch (IOException e) {
-//            LOG.info(e.getMessage());
-//        }
-//    }
-//
-//    protected static void setUpEnvironmentURLFor(String key) {
-//        String value = getRunProps().getProperty(key);
-//        LOG.warn("Properties : key  " + key +" value :" + value);
-//
-//        if (StringUtils.startsWith(value, "http://")) {
-//            return;
-//        }
-//        String urlFromVMOptions = System.getProperty(key);
-//        if (null != urlFromVMOptions) {
-//
-//            LoadProperties.getRunProps().put(key, urlFromVMOptions);
-//        }
-//    }
-//}
-import lombok.Getter;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Collections;
 import java.util.Properties;
 
 public class LoadProperties {
     private static final Logger LOG = LoggerFactory.getLogger(LoadProperties.class);
 
-    @Getter
     private static final Properties runProps = new Properties();
 
-    public static void loadRunConfigProps(String configPropertyFileLocation) {
+    /**
+     * Loads the environment properties from the given file location.
+     */
+    public static synchronized void loadRunConfigProps(String configPropertyFileLocation) {
         try (InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(configPropertyFileLocation)) {
             if (inputStream == null) {
                 throw new IOException("Properties file not found: " + configPropertyFileLocation);
             }
+            runProps.clear();
             runProps.load(inputStream);
             setUpEnvironmentURLFor("site.url");
             setUpEnvironmentURLFor("api.url");
@@ -79,6 +35,9 @@ public class LoadProperties {
         }
     }
 
+    /**
+     * Gets a property value, or returns the default if not found.
+     */
     public static String getProperty(String key, String defaultValue) {
         if (runProps.isEmpty()) {
             throw new IllegalStateException("Properties file not loaded. Call loadRunConfigProps() first.");
@@ -86,26 +45,30 @@ public class LoadProperties {
         return runProps.getProperty(key, defaultValue);
     }
 
+    /**
+     * Ensures environment URLs are set up, possibly overridden by VM options.
+     */
     protected static void setUpEnvironmentURLFor(String key) {
         String value = getProperty(key, null);
         if (value == null) {
             LOG.warn("Property '{}' not found. Skipping setup for this key.", key);
             return;
         }
-        LOG.warn("Properties: key '{}' value '{}'", key, value);
+        LOG.info("Properties: key '{}' value '{}'", key, value);
 
         if (!StringUtils.startsWith(value, "http://")) {
             String urlFromVMOptions = System.getProperty(key);
             if (urlFromVMOptions != null) {
                 runProps.put(key, urlFromVMOptions);
+                LOG.info("Overriding property '{}' with VM option value '{}'", key, urlFromVMOptions);
             }
         }
     }
 
+    /**
+     * Returns the loaded properties.
+     */
     public static Properties getRunProps() {
-        Properties unmodifiableProps = new Properties();
-        unmodifiableProps.putAll(Collections.unmodifiableMap(runProps));
-        return unmodifiableProps;
-
+        return runProps;
     }
 }
